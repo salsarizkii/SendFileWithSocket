8106878473350bf4ddf60bf8a83a753b86b60347
salsarizkii <salsarizki.srs@gmail.com>
2025-03-20 12:10:10 +0700

    fiks some part

--- sender.py
+++ sender.py
@@ -1,7 +1,7 @@
 import os, socket, time
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.bind((socket.gethostbyname(socket.gethostname()), 22222))
+sock.bind((socket.gethostbyname(socket.gethostname()), 8000))
 sock.listen(5)
 print("HOST: ", sock.getsockname())
 
@@ -10,21 +10,13 @@ client, addr = sock.accept()
 file_name = input("File name: ")
 file_size = os.path.getsize(file_name)
 
-client.send(file_name.encode().ljust(100))  # Ensure fixed-size name transmission
+client.send(file_name.encode().ljust(100))  # Ensure fixed-size name transmission3
 client.send(str(file_size).encode().ljust(100))  # Ensure proper size transmission
 
 with open(file_name, "rb") as file:
      c = 0
 
      start_time = time.time()
-    # while c <= file_size:
-     #    data = file.read(1024)
-      #   if not (data):
-       #      break
-     #client.sendall(data)
-     #c += len(data)
-
-    # end_time = time.time()
 
      while c < file_size:  # Corrected condition
         data = file.read(1024)  # Read up to 1024 bytes
@@ -35,6 +27,7 @@ with open(file_name, "rb") as file:
 
         end_time = time.time()
 
-print("file transfer complete. Total time: ", end_time - start_time)
+
+print("File berhasil di transfer. Total waktu yang dibutuhkan: ", end_time - start_time)
 
 sock.close()
